f299aca5ef3f0e8092534e87334e2e3b95e98c5b
superhero <firefox1215@126.com>
2019-08-15 22:07:33 +0800

    update .gitignore file

---
+++ week-1/code/Percolation.java
@@ -0,0 +1,42 @@
+/**
+ * Performance requirements.
+ * 1. The constructor should take time proportional to n2;
+ * 2. all methods should take constant time plus a constant number of calls to the union–find methods union(), find(), connected(), and count().
+ */
+public class Percolation {
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        return false;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        return false;
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        return 0;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return false;
+    }
+
+    // test client (optional)
+    public static void main(String[] args) {
+
+    }
+}
