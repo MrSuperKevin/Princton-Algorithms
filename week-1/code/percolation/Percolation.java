c8a327c921f3871c596bf3dd3ba3870da8a9230d
superhero <firefox1215@126.com>
2019-08-18 23:04:48 +0800

    format code

--- week-1/code/percolation/Percolation.java
+++ week-1/code/percolation/Percolation.java
@@ -8,101 +8,101 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-    private int size;
-    private int openCount;
-    // 1 if the site on the coordinate is opened 
-    private int[][] grid;
-    private WeightedQuickUnionUF weightedQuickUnionUF;
+	private int size;
+	private int openCount;
+	// 1 if the site on the coordinate is opened
+	private int[][] grid;
+	private WeightedQuickUnionUF weightedQuickUnionUF;
 
-    // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n) {
-        if (n <= 0) {
-            throw new IllegalArgumentException();
-        }
+	// creates n-by-n grid, with all sites initially blocked
+	public Percolation(int n) {
+		if (n <= 0) {
+			throw new IllegalArgumentException();
+		}
 
-        size = n;
-        openCount = 0;
-        grid = new int[n][n];
-        // put one virtual point in the head and one in the tail of the array in unionFind, and connect the head with grid[0][*], connect the tail with grid[size - 1][*]
-        weightedQuickUnionUF = new WeightedQuickUnionUF(n * n + 2);
-    }
+		size = n;
+		openCount = 0;
+		grid = new int[n][n];
+		// put one virtual point in the head and one in the tail of the array in unionFind, and connect the head with grid[0][*], connect the tail with grid[size - 1][*]
+		weightedQuickUnionUF = new WeightedQuickUnionUF(n * n + 2);
+	}
 
-    // test client (optional)
-    public static void main(String[] args) {
+	// test client (optional)
+	public static void main(String[] args) {
 
-    }
+	}
 
-    // opens the site (row, col) if it is not open already
-    public void open(int row, int col) {
-        checkLegalArguments(row);
-        checkLegalArguments(col);
+	// opens the site (row, col) if it is not open already
+	public void open(int row, int col) {
+		checkLegalArguments(row);
+		checkLegalArguments(col);
 
-        if (grid[row - 1][col - 1] == 0) {
-            grid[row - 1][col - 1] = 1;
-            openCount++;
+		if (grid[row - 1][col - 1] == 0) {
+			grid[row - 1][col - 1] = 1;
+			openCount++;
 
-            // connect sites around the new open site
-            if (row > 1) {
-                if (isOpen(row - 1, col)) {
-                    weightedQuickUnionUF.union(calIndexOfArray(row, col), calIndexOfArray(row - 1, col));
-                }
-            } else {
-                weightedQuickUnionUF.union(calIndexOfArray(row, col), 0);
-            }
+			// connect sites around the new open site
+			if (row > 1) {
+				if (isOpen(row - 1, col)) {
+					weightedQuickUnionUF.union(calIndexOfArray(row, col), calIndexOfArray(row - 1, col));
+				}
+			} else {
+				weightedQuickUnionUF.union(calIndexOfArray(row, col), 0);
+			}
 
-            if (row < size) {
-                if (isOpen(row + 1, col)) {
-                    weightedQuickUnionUF.union(calIndexOfArray(row, col), calIndexOfArray(row + 1, col));
-                }
-            } else {
-                weightedQuickUnionUF.union(calIndexOfArray(row, col), size * size + 1);
-            }
+			if (row < size) {
+				if (isOpen(row + 1, col)) {
+					weightedQuickUnionUF.union(calIndexOfArray(row, col), calIndexOfArray(row + 1, col));
+				}
+			} else {
+				weightedQuickUnionUF.union(calIndexOfArray(row, col), size * size + 1);
+			}
 
-            if (col > 1 && isOpen(row, col - 1)) {
-                weightedQuickUnionUF.union(calIndexOfArray(row, col), calIndexOfArray(row, col - 1));
-            }
-            if (col < size && isOpen(row, col + 1)) {
-                weightedQuickUnionUF.union(calIndexOfArray(row, col), calIndexOfArray(row, col + 1));
-            }
-        }
-    }
+			if (col > 1 && isOpen(row, col - 1)) {
+				weightedQuickUnionUF.union(calIndexOfArray(row, col), calIndexOfArray(row, col - 1));
+			}
+			if (col < size && isOpen(row, col + 1)) {
+				weightedQuickUnionUF.union(calIndexOfArray(row, col), calIndexOfArray(row, col + 1));
+			}
+		}
+	}
 
-    // calculate the index of array in union find class, skip one in the head
-    private int calIndexOfArray(int row, int col) {
-        return (row - 1) * size + col;
-    }
+	// calculate the index of array in union find class, skip one in the head
+	private int calIndexOfArray(int row, int col) {
+		return (row - 1) * size + col;
+	}
 
-    // is the site (row, col) open?
-    public boolean isOpen(int row, int col) {
-        checkLegalArguments(row);
-        checkLegalArguments(col);
+	// is the site (row, col) open?
+	public boolean isOpen(int row, int col) {
+		checkLegalArguments(row);
+		checkLegalArguments(col);
 
-        return grid[row - 1][col - 1] == 1;
-    }
+		return grid[row - 1][col - 1] == 1;
+	}
 
-    // is the site (row, col) full? means if the top to here percolates
-    public boolean isFull(int row, int col) {
-        checkLegalArguments(row);
-        checkLegalArguments(col);
+	// is the site (row, col) full? means if the top to here percolates
+	public boolean isFull(int row, int col) {
+		checkLegalArguments(row);
+		checkLegalArguments(col);
 
-        // 网格的头顶点和该点的连通性
-        return weightedQuickUnionUF.connected(0, calIndexOfArray(row, col));
-    }
+		// 网格的头顶点和该点的连通性
+		return weightedQuickUnionUF.connected(0, calIndexOfArray(row, col));
+	}
 
-    // returns the number of open sites
-    public int numberOfOpenSites() {
-        return openCount;
-    }
+	// returns the number of open sites
+	public int numberOfOpenSites() {
+		return openCount;
+	}
 
-    // does the system percolate?
-    public boolean percolates() {
-        return weightedQuickUnionUF.connected(0, size * size + 1);
-    }
+	// does the system percolate?
+	public boolean percolates() {
+		return weightedQuickUnionUF.connected(0, size * size + 1);
+	}
 
-    // check input index, should between 1~n
-    private void checkLegalArguments(int n) {
-        if (n <= 0 || n > size) {
-            throw new IllegalArgumentException(String.format("Illegal argument: %d", n));
-        }
-    }
+	// check input index, should between 1~n
+	private void checkLegalArguments(int n) {
+		if (n <= 0 || n > size) {
+			throw new IllegalArgumentException(String.format("Illegal argument: %d", n));
+		}
+	}
 }
--- week-1/code/percolation/PercolationStats.java
+++ week-1/code/percolation/PercolationStats.java
@@ -10,75 +10,75 @@ import java.util.Random;
 
 public class PercolationStats {
 
-    private double[] threshold;
+	private double[] threshold;
 
-    // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials) {
-        if (n <= 0 || trials <= 0) {
-            throw new IllegalArgumentException();
-        }
+	// perform independent trials on an n-by-n grid
+	public PercolationStats(int n, int trials) {
+		if (n <= 0 || trials <= 0) {
+			throw new IllegalArgumentException();
+		}
 
-        threshold = new double[trials];
-        Random random = new Random();
-        for (int i = 0; i < trials; i++) {
-            Percolation percolation = new Percolation(n);
+		threshold = new double[trials];
+		Random random = new Random();
+		for (int i = 0; i < trials; i++) {
+			Percolation percolation = new Percolation(n);
 
-            while (!percolation.percolates()) {
-                int row = random.nextInt(n) + 1;
-                int col = random.nextInt(n) + 1;
-                if (percolation.isOpen(row, col)) {
-                    continue;
-                }
+			while (!percolation.percolates()) {
+				int row = random.nextInt(n) + 1;
+				int col = random.nextInt(n) + 1;
+				if (percolation.isOpen(row, col)) {
+					continue;
+				}
 
-                percolation.open(row, col);
-            }
+				percolation.open(row, col);
+			}
 
-            threshold[i] = percolation.numberOfOpenSites() * 1.0 / (n * n);
-        }
-    }
+			threshold[i] = percolation.numberOfOpenSites() * 1.0 / (n * n);
+		}
+	}
 
-    // test client (see below)
-    public static void main(String[] args) {
-        Stopwatch stopwatch = new Stopwatch();
-        int n = 200, trials = 10000;
-        PercolationStats percolationStats = new PercolationStats(n, trials);
-        System.out.println("Experiment status");
-        System.out.println("-----------------------------------");
-        System.out.println(String.format("Percolation size: %d, trial num: %d", n, trials));
-        System.out.println(String.format("Total time: %fs", stopwatch.elapsedTime()));
-        System.out.println(String.format("Mean of percolation threshold: %f", percolationStats.mean()));
-        System.out.println(String.format("Standard deviation of percolation threshold: %f", percolationStats.stddev()));
-        System.out.println(String.format("Endpoint of 95 confidence interval: %f ~ %f", percolationStats.confidenceLo(), percolationStats.confidenceHi()));
-    }
+	// test client (see below)
+	public static void main(String[] args) {
+		Stopwatch stopwatch = new Stopwatch();
+		int n = 200, trials = 10000;
+		PercolationStats percolationStats = new PercolationStats(n, trials);
+		System.out.println("Experiment status");
+		System.out.println("-----------------------------------");
+		System.out.println(String.format("Percolation size: %d, trial num: %d", n, trials));
+		System.out.println(String.format("Total time: %fs", stopwatch.elapsedTime()));
+		System.out.println(String.format("Mean of percolation threshold: %f", percolationStats.mean()));
+		System.out.println(String.format("Standard deviation of percolation threshold: %f", percolationStats.stddev()));
+		System.out.println(String.format("Endpoint of 95 confidence interval: %f ~ %f", percolationStats.confidenceLo(), percolationStats.confidenceHi()));
+	}
 
-    // sample mean of percolation threshold
-    public double mean() {
-        double sum = 0.0;
-        for (int i = 0; i < threshold.length; i++) {
-            sum += threshold[i];
-        }
-        return sum / threshold.length;
-    }
+	// sample mean of percolation threshold
+	public double mean() {
+		double sum = 0.0;
+		for (int i = 0; i < threshold.length; i++) {
+			sum += threshold[i];
+		}
+		return sum / threshold.length;
+	}
 
-    // sample standard deviation of percolation threshold
-    public double stddev() {
-        double mean = mean();
-        double sum = 0.0;
-        for (int i = 0; i < threshold.length; i++) {
-            sum += (threshold[i] - mean) * (threshold[i] - mean);
-        }
+	// sample standard deviation of percolation threshold
+	public double stddev() {
+		double mean = mean();
+		double sum = 0.0;
+		for (int i = 0; i < threshold.length; i++) {
+			sum += (threshold[i] - mean) * (threshold[i] - mean);
+		}
 
-        return sum * 1.0 / (threshold.length - 1);
-    }
+		return sum * 1.0 / (threshold.length - 1);
+	}
 
-    // low endpoint of 95% confidence interval
-    public double confidenceLo() {
-        return mean() - 1.96 * stddev() / Math.sqrt(threshold.length);
-    }
+	// low endpoint of 95% confidence interval
+	public double confidenceLo() {
+		return mean() - 1.96 * stddev() / Math.sqrt(threshold.length);
+	}
 
-    // high endpoint of 95% confidence interval
-    public double confidenceHi() {
-        return mean() + 1.96 * stddev() / Math.sqrt(threshold.length);
-    }
+	// high endpoint of 95% confidence interval
+	public double confidenceHi() {
+		return mean() + 1.96 * stddev() / Math.sqrt(threshold.length);
+	}
 
 }
